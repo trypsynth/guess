f290655ec568bb114d5a74d3c3cd7c0cbdc28180
Quin Gillespie <trypsynth@gmail.com>
2026-06-23 20:32:02 -0600

    deinit Cli, print a message if no results are found.

--- src/main.zig
+++ src/main.zig
@@ -5,8 +5,13 @@ const guessers = @import("guessers.zig");
 pub fn main(init: std.process.Init) !void {
 	const allocator = init.gpa;
 	const cli = try Cli.parse(allocator, init.minimal.args);
+	defer cli.deinit(allocator);
 	const results = try guessers.guess(allocator, cli.input);
 	defer allocator.free(results);
+	if (results.len == 0) {
+		std.debug.print("No possible matches.\n", .{});
+		std.process.exit(1);
+	}
 	for (results) |result| {
 		std.debug.print("{s}, {d}% confidence\n", .{ result.type.displayName(), result.confidence });
 	}
